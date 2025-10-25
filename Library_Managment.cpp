e5b96b75e0e7737e9bd1ca29dbff82426607ea73
iliyaasghari <iliya571524@gmail.com>
2025-10-25 23:01:35 +0330

    Repeatability

--- Library_Managment.cpp
+++ Library_Managment.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 #include<conio.h>
-
+#include<vector>
+#include<iomanip>
 using namespace std;
 class Book
 {
     private:
         string Title;
         string Author;
-        bool Is_Available;
     public:
         void Set_Title()
         {
@@ -40,7 +40,85 @@ class Book
             return Author;
         }
 };
+class Managment
+{
+    private:
+        vector <Book> book;
+        Book b;
+    public:
+        void Add()
+        {
+            b.Set_Title();
+            b.Set_Author();
+            book.push_back(b);
+        }
+`       void Search()
+        {
+            
+        }
+
+        void Show()
+        {
+            cout<<setw(20)<<"Book title "<<setw(20)<<"Book Author "<<endl;
+            for(int i=0; i<static_cast<int>(book.size()); i++)
+            {
+                cout<<i+1<<")"<<setw(15)<<book[i].Get_Title()<<setw(20)<<book[i].Get_Author()<<endl;
+            }
+        }
+
+};
+bool End()
+{
+    string input;
+    cout<<"Do you want to continue?"<<endl;
+    getline(cin>>ws,input);
+    if(input=="yes" || input=="Yes")
+        {
+            return true;
+        }
+    return false;
+}
 int main()
 {
-
+    Managment m;
+    enum Menu { Add=1 , Search=2 , Remove=3 , Show=4 , Save=5 , Exit=6};
+    int o;
+    while(true)
+    {
+        system("cls");
+        cout<<"1)Add"<<endl<<"2)Search"<<endl<<"3)Remove"<<endl<<"4)Show"<<endl<<"5)Save"<<endl<<"6)Exit"<<endl;
+        cout<<string(40 , '=');
+        cout<<endl<<"Enter your option number:";
+        cin>>o;
+        switch (o)
+        {
+        case Add:
+            system("cls");
+            m.Add();
+            break;
+        case Search:   
+            system("cls");
+            break;
+        case Remove:
+            system("cls");
+            break;
+        case Show:
+            system("cls");
+            m.Show();
+            break;
+        case Save:
+            system("cls");
+            break;
+        case Exit:
+            cout<<"Exiting....."<<endl;
+            return 0;
+        default:
+            cout<<"Invalid input";
+        }
+        if(End()==false)
+            {
+                cout<<"Exiting....."<<endl;
+                return 0;
+            }
+    }
 }
