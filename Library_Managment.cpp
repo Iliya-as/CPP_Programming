71f47e666fac60fc9339d8c0f5fe7aa2132d5d0f
iliyaasghari <iliya571524@gmail.com>
2025-10-29 10:03:04 +0330

    Complete search function

--- Library_Managment.cpp
+++ Library_Managment.cpp
@@ -1,103 +1,185 @@
-#include<iostream>
-#include<conio.h>
-#include<vector>
-#include<iomanip>
+// shift+alt+f=formating
+#include <iostream>
+#include <conio.h>
+#include <vector>
+#include <iomanip>
 using namespace std;
 class Book
 {
-    private:
-        string Title;
-        string Author;
-    public:
-        void Set_Title()
+private:
+    string Title;
+    string Author;
+
+public:
+    void Set_Title()
+    {
+        string title;
+        do
         {
-            string title;
-            do
-            {
-                cout<<"Enter the book title:";
-                getline(cin>>ws,title);
-            }
-            while(title.empty());
-            Title=title;
-        }
-        string Get_Title()
+            cout << "Enter the book title:";
+            getline(cin >> ws, title);
+        } while (title.empty());
+        Title = title;
+    }
+    string Get_Title()
+    {
+        return Title;
+    }
+    void Set_Author()
+    {
+        string author;
+        do
         {
-            return Title;
-        }
-        void Set_Author()
-        {
-             string author;
-            do
-            {
-                cout<<"Enter the book Author:";
-                getline(cin>>ws,author);
-            }
-            while(author.empty());
-            Author=author;
-        }
-        string Get_Author()
-        {
-            return Author;
-        }
+            cout << "Enter the book Author:";
+            getline(cin >> ws, author);
+        } while (author.empty());
+        Author = author;
+    }
+    string Get_Author()
+    {
+        return Author;
+    }
 };
 class Managment
 {
-    private:
-        vector <Book> book;
-        Book b;
-    public:
-        void Add()
-        {
-            b.Set_Title();
-            b.Set_Author();
-            book.push_back(b);
-        }
-`       void Search()
-        {
-            
-        }
+private:
+    vector<Book> book;
+    Book b;
 
-        void Show()
+public:
+    void Add()
+    {
+        b.Set_Title();
+        b.Set_Author();
+        book.push_back(b);
+    }
+    void Search()
+    {
+        cout << "1)Search by book name" << setw(25) << "2)Search by author" << endl;
+        cout << "Enter your choice:";
+        int ch;
+        vector<int> hold;
+        cin >> ch;
+        cin.ignore();
+        switch (ch)
         {
-            cout<<setw(20)<<"Book title "<<setw(20)<<"Book Author "<<endl;
-            for(int i=0; i<static_cast<int>(book.size()); i++)
+        case 1:
+        {
+            string bn;
+            cout << "Enter the book name:";
+            getline(cin >> ws, bn);
+            for (int i = 0; i < static_cast<int>(book.size()); i++)
             {
-                cout<<i+1<<")"<<setw(15)<<book[i].Get_Title()<<setw(20)<<book[i].Get_Author()<<endl;
+                if (bn == book[i].Get_Title())
+                {
+                    hold.push_back(i);
+                }
+                else
+                {
+                    continue;
+                }
             }
+            if (hold.empty())
+            {
+                cout << "Book dosen't exist" << endl;
+            }
+            else
+            {
+                cout << setw(20) << "i" << setw(20) << "Book Author " << endl;
+                for (int i = 0; i < static_cast<int>(hold.size()); i++)
+                {
+                    cout << setw(18) << hold[i] + 1 << setw(20) << book[hold[i]].Get_Author() << endl;
+                }
+            }
+            break;
         }
-
+        case 2:
+        {
+            string an;
+            cout << "Enter the author name:";
+            getline(cin >> ws, an);
+            for (int i = 0; i < static_cast<int>(book.size()); i++)
+            {
+                if (an == book[i].Get_Author())
+                {
+                    hold.push_back(i);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            if (hold.empty())
+            {
+                cout << "author dosen't exist";
+            }
+            else
+            {
+                cout << setw(20) << "i" << setw(20) << "Book name" << endl;
+                for (int i = 0; i < static_cast<int>(hold.size()); i++)
+                {
+                    cout << setw(18) << hold[i] + 1 << setw(20) << book[hold[i]].Get_Title() << endl;
+                }
+            }
+            break;
+        }
+        }
+    }
+    void Show()
+    {
+        cout << setw(20) << "Book title " << setw(20) << "Book Author " << endl;
+        for (int i = 0; i < static_cast<int>(book.size()); i++)
+        {
+            cout << i + 1 << ")" << setw(15) << book[i].Get_Title() << setw(20) << book[i].Get_Author() << endl;
+        }
+    }
 };
 bool End()
 {
     string input;
-    cout<<"Do you want to continue?"<<endl;
-    getline(cin>>ws,input);
-    if(input=="yes" || input=="Yes")
-        {
-            return true;
-        }
+    cout << "Do you want to continue?" << endl;
+    getline(cin >> ws, input);
+    if (input == "yes" || input == "Yes")
+    {
+        return true;
+    }
     return false;
 }
 int main()
 {
     Managment m;
-    enum Menu { Add=1 , Search=2 , Remove=3 , Show=4 , Save=5 , Exit=6};
+    enum Menu
+    {
+        Add = 1,
+        Search = 2,
+        Remove = 3,
+        Show = 4,
+        Save = 5,
+        Exit = 6
+    };
     int o;
-    while(true)
+    while (true)
     {
         system("cls");
-        cout<<"1)Add"<<endl<<"2)Search"<<endl<<"3)Remove"<<endl<<"4)Show"<<endl<<"5)Save"<<endl<<"6)Exit"<<endl;
-        cout<<string(40 , '=');
-        cout<<endl<<"Enter your option number:";
-        cin>>o;
+        cout << "1)Add" << endl
+             << "2)Search" << endl
+             << "3)Remove" << endl
+             << "4)Show" << endl
+             << "5)Save" << endl
+             << "6)Exit" << endl;
+        cout << string(40, '=');
+        cout << endl
+             << "Enter your option number:";
+        cin >> o;
         switch (o)
         {
         case Add:
             system("cls");
             m.Add();
             break;
-        case Search:   
+        case Search:
             system("cls");
+            m.Search();
             break;
         case Remove:
             system("cls");
@@ -110,15 +192,15 @@ int main()
             system("cls");
             break;
         case Exit:
-            cout<<"Exiting....."<<endl;
+            cout << "Exiting....." << endl;
             return 0;
         default:
-            cout<<"Invalid input";
+            cout << "Invalid input";
         }
-        if(End()==false)
-            {
-                cout<<"Exiting....."<<endl;
-                return 0;
-            }
+        if (End() == false)
+        {
+            cout << "Exiting....." << endl;
+            return 0;
+        }
     }
 }
